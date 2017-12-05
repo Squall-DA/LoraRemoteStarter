0aa6b2cb2ca207622be8e84554ac75d19dc73eba
Daniel Huelsman <Squall-DA@users.noreply.github.com>
2017-12-04 20:23:07 -0500

    Add initial main.cpp and include extra libraries

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+/** 
+ *  @file                   main.cpp
+ *  @brief                  Main source file for remote starter
+ *  @copyright              2017 Squall-DA
+ *  @date                   12/04/2017
+ * 
+ *  @remark Author:         Squall-DA
+ *  @remark Project Tree:   BlynkRemoteStarter
+ * 
+ */
+
+/* Includes */
+#include <Arduino.h>
+#include <TinyGsmClient.h>
+
+/*========================================================================* 
+ *  SECTION - External variables that cannot be defined in header files   * 
+ *========================================================================* 
+ */
+
+/*========================================================================* 
+ *  SECTION - Local function prototypes                                   * 
+ *========================================================================* 
+ */
+
+/*========================================================================* 
+ *  SECTION - Local variables                                             * 
+ *========================================================================* 
+ */
+
+/**
+ *  @fn     void setup()
+ *
+ *  @brief  Runs once at device startup. Is usually used to configure IO
+ *  
+ *  @return N/A
+ *
+ *  @author Squall-DA
+ *
+ *  @note   N/A
+ *
+ */
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+/**
+ *  @fn     void loop()
+ *
+ *  @brief  NULL loop of the device. Code placed in this functions
+ *          runs as fast as the microcontroller will allow it.
+ *  
+ *  @return N/A
+ *
+ *  @author Squall-DA
+ *
+ *  @note   N/A
+ *
+ */
+void loop() {
+    // put your main code here, to run repeatedly:
+}
