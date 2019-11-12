f1ebe2c656ae53d06c54bb344b8f4cae8951d35e
Squall-DA <Squall-DA@users.noreply.github.com>
2019-11-12 08:37:00 -0500

    Update project
    Update the project to use Lora on a TTGO Lora 32.
    Update gitignore to ignore ".pio".
    Update the README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Remote Starter
 
 ### Overview
-Remote Starter is an Arduino project to create a cellular remote starter for vehicles. This initial version will use an Arduino uno with a relay and GPRS shield to control an extra keyfob with remote start. Blynk will be used for the communication layer. This is an Arduino project using PlatformIO for development.
+Remote Starter is an Arduino project to create a Lora based remote starter for a vehicle. This project will be based around the TTGO LORA 32 for ease of use. 
--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,6 @@
 #include "main.h"
 
 #include <stdint.h>
-#include <TinyGsmClient.h>
-#include <BlynkSimpleSIM800.h>
-#include <SoftwareSerial.h>
 #include "Keyfob.h"
 
 
@@ -33,21 +30,6 @@
  *  SECTION - Local variables                                             * 
  *========================================================================* 
  */
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "a28e9d33d7814e58ac7ff904fa19b53e";
-
-// Your GPRS credentials
-// Leave empty, if missing user or pass
-char apn[]  = "YourAPN";
-char user[] = "";
-char pass[] = "";
-
-/* Use software serial on UNO */
-SoftwareSerial SerialAT(7, 8); // RX, TX
-
-/* Set Tiny GSM modem to use software serial */
-TinyGsm modem(SerialAT);
 
 /* Define the vehicle keyfob object 
 *  I am using the lock, unlock, and start keyfob
@@ -71,23 +53,11 @@ Keyfob ramKeyfob(START_PIN,UNLOCK_PIN,LOCK_PIN);
 void setup() 
 { 
     // Debug console
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     delay(10);
 
-    // Set GSM module baud rate
-    SerialAT.begin(115200);
     delay(3000);
-
-    // Restart takes quite some time
-    // To skip it, call init() instead of restart()
-    Serial.println("Initializing modem...");
-    modem.restart();
-
-    // Unlock your SIM card with a PIN
-    //modem.simUnlock("1234");
-
-    Blynk.begin(auth, modem, apn, user, pass);
 }
 
 /**
@@ -105,72 +75,5 @@ void setup()
  */
 void loop() 
 {
-    Blynk.run();
     ramKeyfob.vRun();
-}
-
-/**
- *  @fn     void BLYNK_WRITE(V0)
- *
- *  @brief  #define from Blynk library. Called whenever Virtual pin 0
- *          is written to by the Blynk widget. 
- *  
- *  @return N/A
- *
- *  @author Squall-DA
- *
- *  @note   Virtual pin 0 controls the remote start on the keyfob
- *
- */
-BLYNK_WRITE(V0)
-{
-    if( param.asInt() )
-    {
-        ramKeyfob.vStartVehicle();
-        Blynk.virtualWrite(V0,0);
-    }
-}
-
-/**
- *  @fn     void BLYNK_WRITE(V1)
- *
- *  @brief  #define from Blynk library. Called whenever virtual pin 1
- *          is written to by the Blynk widget. 
- *  
- *  @return N/A
- *
- *  @author Squall-DA
- *
- *  @note   Virtual pin 1 controls the lock on the keyfob
- *
- */
-BLYNK_WRITE(V1)
-{
-    if( param.asInt() )
-    {
-        ramKeyfob.vLockVehicle();
-        Blynk.virtualWrite(V1,0);
-    }
-}
-
-/**
- *  @fn     void BLYNK_WRITE(V2)
- *
- *  @brief  #define from Blynk library. Called whenever virtual pin 2
- *          is written to by the Blynk widget. 
- *  
- *  @return N/A
- *
- *  @author Squall-DA
- *
- *  @note   Virtual pin 2 controls the unlock on the keyfob
- *
- */
-BLYNK_WRITE(V2)
-{
-    if( param.asInt() )
-    {
-        ramKeyfob.vUnlockVehicle();
-        Blynk.virtualWrite(V2,0);
-    }
 }
